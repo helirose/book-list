d605f8213af2ddc015ddc87ac39404a009309263
Charlotte Rees <creative.flare@gmail.com>
2022-02-19 21:11:42 +0000

    tidied template files for new structure

---
+++ src/components/DragBox.vue
@@ -0,0 +1,19 @@
+<script setup>
+defineProps({
+  msg: {
+    type: String,
+    required: true
+  }
+})
+</script>
+
+<template>
+  <div class="greetings">
+    <h1>The Book List</h1>
+    Text here
+  </div>
+</template>
+
+<style scoped>
+
+</style>
