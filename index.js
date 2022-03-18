b0a6c6e26e9ef989623278d016faffc06e056cba
Charlotte Rees <creative.flare@gmail.com>
2022-03-18 23:22:56 +0000

    added env example and database connection

--- index.js
+++ index.js
@@ -5,12 +5,18 @@
 
 const express = require('express');
 const cors = require('cors');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
 const app = express();
 const port = process.env.PORT || 4000;
 
 app.use(cors());
 
 require('./src/routes/bookRoutes.js') (app);
+
+require('./src/config/database.js');
 
 app.listen(port, () => {
     console.log(`listening on ${port}`);
---
+++ src/config/database.js
@@ -0,0 +1,18 @@
+const mysql = require('mysql2');
+
+const connection = mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME
+});
+
+connection.connect();
+
+connection.query('SELECT 1 + 1 AS solution', (err, rows, fields) => {
+  if (err) throw err
+
+  console.log('The solution is: ', rows[0].solution)
+});
+
+connection.end();
